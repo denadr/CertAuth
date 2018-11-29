8ec1285d993a42f648d690277e9699532863f85f
Dennis Adrians <dennisadrians@t-online.de>
2018-11-29 23:01:30 +0100

    initial commit

--- Client/Program.cs
+++ Client/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Net.Http;
+using System.Security.Cryptography.X509Certificates;
 
 namespace Client
 {
@@ -6,7 +8,47 @@ namespace Client
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var certificate = GetCertificate();
+
+            using (var httpHandler = new HttpClientHandler()
+            {
+                ClientCertificateOptions = ClientCertificateOption.Manual,
+                UseProxy = false
+            })
+            {
+                httpHandler.ServerCertificateCustomValidationCallback = (req, cert, chain, err) => true;
+                httpHandler.ClientCertificates.Add(certificate);
+
+                using (var client = new HttpClient(httpHandler)
+                {
+                    BaseAddress = new Uri("https://localhost:44368")
+                })
+                {
+                    using (var response = client.GetAsync("/api/values").Result)
+                    {
+                        response.EnsureSuccessStatusCode();
+                    }
+                }
+            }
+        }
+
+        static X509Certificate GetCertificate()
+        {
+            X509Store certificateStore = null;
+            try
+            {
+                certificateStore = new X509Store(StoreName.My, StoreLocation.LocalMachine, OpenFlags.OpenExistingOnly | OpenFlags.ReadOnly);
+
+                var certificates = certificateStore.Certificates.Find(X509FindType.FindBySerialNumber, "3a6cc87f931ceda2422abc0d6e8edba0", true);
+
+                var certificate = certificates.Count == 0 ? null : certificates[0];
+
+                return certificate;
+            }
+            finally
+            {
+                certificateStore?.Close();
+            }
         }
     }
 }
---
+++ Server/Middleware/CertificateAuthenticationMiddleware.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Net;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading.Tasks;
+
+namespace Server.Middleware
+{
+    public class CertificateAuthenticationMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public CertificateAuthenticationMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            try
+            {
+                var certificateHeader = context.Request.Headers["X-ARR-ClientCert"];
+
+                var certificate = new X509Certificate2(Convert.FromBase64String(certificateHeader));
+
+                if (ValidateCertificate(certificate))
+                {
+                    await _next.Invoke(context);
+                }
+                else
+                {
+                    context.Response.StatusCode = (int)HttpStatusCode.Unauthorized;
+                }
+            }
+            catch (Exception)
+            {
+                context.Response.StatusCode = (int)HttpStatusCode.Unauthorized;
+            }
+        }
+
+        private bool ValidateCertificate(X509Certificate2 certificate)
+        {
+            return true;
+        }
+    }
+
+    public static class CertificateAuthenticationMiddlewareExtensions
+    {
+        public static IApplicationBuilder UseCertificateAuthentication(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<CertificateAuthenticationMiddleware>();
+        }
+    }
+}
